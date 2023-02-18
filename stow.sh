f178ae4d923e8d8643732efe39d6f826fdad4c68
groveer <groveer@outlook.com>
2023-02-19 01:54:45 +0800

    feat: support dynamic wallpaper

--- stow.sh
+++ stow.sh
@@ -1,63 +1,63 @@
 #!/bin/sh
 
 if [ ! -x "/usr/bin/stow" ]; then
-	exit
+    exit
 fi
 
 if [ -x "/usr/bin/Hyprland" ]; then
-	target=hypr
-	configdir="${HOME}/.config/${target}"
-	[ ! -d "${configdir}" ] && mkdir -p "${configdir}"
-	stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
+    target=hypr
+    configdir="${HOME}/.config/${target}"
+    [ ! -d "${configdir}" ] && mkdir -p "${configdir}"
+    stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
 fi
 
 if [ -x "/usr/bin/nvim" ]; then
-	target=nvim
-	configdir="${HOME}/.config/${target}"
-	[ ! -d "${configdir}" ] && mkdir -p "${configdir}"
-	stow -R "${target}" -t "${configdir}" --ignore ".github" && echo "stow ${target} done!"
+    target=nvim
+    configdir="${HOME}/.config/${target}"
+    [ ! -d "${configdir}" ] && mkdir -p "${configdir}"
+    stow -R "${target}" -t "${configdir}" --ignore ".github" && echo "stow ${target} done!"
 fi
 
 if [ -x "/usr/bin/sway" ]; then
-	target=sway
-	configdir="${HOME}/.config/${target}"
-	[ ! -d "${configdir}" ] && mkdir -p "${configdir}"
-	stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
+    target=sway
+    configdir="${HOME}/.config/${target}"
+    [ ! -d "${configdir}" ] && mkdir -p "${configdir}"
+    stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
 fi
 
 if [ -x "/usr/bin/waybar" ]; then
-	target=waybar
-	configdir="${HOME}/.config/${target}"
-	[ ! -d "${configdir}" ] && mkdir -p "${configdir}"
-	stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
+    target=waybar
+    configdir="${HOME}/.config/${target}"
+    [ ! -d "${configdir}" ] && mkdir -p "${configdir}"
+    stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
 fi
 
 if [ -x "/usr/bin/wezterm" ]; then
-	target=wezterm
-	configdir="${HOME}/.config/${target}"
-	[ ! -d "${configdir}" ] && mkdir -p "${configdir}"
-	stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
+    target=wezterm
+    configdir="${HOME}/.config/${target}"
+    [ ! -d "${configdir}" ] && mkdir -p "${configdir}"
+    stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
 fi
 
 if [ -x "/usr/bin/alacritty" ]; then
-	target=alacritty
-	configdir="${HOME}/.config/${target}"
-	[ ! -d "${configdir}" ] && mkdir -p "${configdir}"
-	stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
+    target=alacritty
+    configdir="${HOME}/.config/${target}"
+    [ ! -d "${configdir}" ] && mkdir -p "${configdir}"
+    stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
 fi
 
 if [ -x "/usr/bin/wofi" ]; then
-	target=wofi
-	configdir="${HOME}/.config/${target}"
-	[ ! -d "${configdir}" ] && mkdir -p "${configdir}"
-	stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
+    target=wofi
+    configdir="${HOME}/.config/${target}"
+    [ ! -d "${configdir}" ] && mkdir -p "${configdir}"
+    stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
 fi
 
 if [ -x "/usr/bin/mpv" ]; then
-	target=mpv
-	configdir="${HOME}/.config/${target}"
-	[ ! -d "${configdir}" ] && mkdir -p "${configdir}"
-	stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
+    target=mpv
+    configdir="${HOME}/.config/${target}"
+    [ ! -d "${configdir}" ] && mkdir -p "${configdir}"
+    stow -R "${target}" -t "${configdir}" && echo "stow ${target} done!"
 fi
 
 if [ -x "/usr/bin/mpv" ]; then
@@ -68,17 +68,26 @@ if [ -x "/usr/bin/mpv" ]; then
 fi
 
 if [ -x "/usr/bin/zsh" ]; then
-	stow -R zsh -t "${HOME}" && echo "stow zsh done!"
+    stow -R zsh -t "${HOME}" && echo "stow zsh done!"
 fi
 
 if [ -f "bg.jpg" ]; then
-	configdir="${HOME}/.local/share/backgrounds"
-	[ ! -d "${configdir}" ] && mkdir "${configdir}"
-	ln -sf "$(pwd)/bg.jpg" "${configdir}/bg.jpg"
+    target=bg.jpg
+    configdir="${HOME}/.local/share/backgrounds"
+    [ ! -d "${configdir}" ] && mkdir "${configdir}"
+    ln -sf "$(pwd)/${target}" "${configdir}/${target}" && echo "ln ${target} done!"
+fi
+
+if [ -f "angry_birds.mov" ]; then
+    target=angry_birds.mov
+    configdir="${HOME}/.local/share/backgrounds"
+    [ ! -d "${configdir}" ] && mkdir "${configdir}"
+    ln -sf "$(pwd)/${target}" "${configdir}/${target}" && echo "ln ${target} done!"
 fi
 
 if [ -x "startde" ]; then
-	configdir="${HOME}/.local/bin"
-	[ ! -d "${configdir}" ] && mkdir "${configdir}"
-	ln -sf "$(pwd)/startde" "${configdir}/startde"
+    target=startde
+    configdir="${HOME}/.local/bin"
+    [ ! -d "${configdir}" ] && mkdir "${configdir}"
+    ln -sf "$(pwd)/${target}" "${configdir}/${target}" && echo "ln ${target} done!"
 fi
