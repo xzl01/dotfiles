b8e3a89d0aef640143d89ad64f26a31d4dca461d
groveer <groveer@outlook.com>
2023-06-06 14:52:28 +0800

    chore(terminal): update font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,18 +1,18 @@
 local wezterm = require("wezterm")
 local font = wezterm.font_with_fallback({
-    "Maple Mono SC NF",
-    "Noto Sans CJK SC",
+	"Maple Mono SC NF",
+	"Noto Sans CJK SC",
 })
 
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.color_scheme = "Dracula (Official)"
 config.font = font
-config.font_size = 13
+config.font_size = 15
 config.enable_scroll_bar = false
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
