ff8a9d0f5f9c2089cdfccc7ad8584427e88f7715
groveer <groveer@outlook.com>
2023-08-25 17:39:59 +0800

    chore(shell): smart to latex command

--- shell_env/50-latex.sh
+++ shell_env/50-latex.sh
@@ -1,7 +1,10 @@
 # need texlive-core package
 if command -v lualatex >/dev/null 2>&1; then
-    PLANTUML_JAR="/usr/share/plantuml/plantuml.jar"
-    [ -f "/usr/share/java/plantuml/plantuml.jar" ] && PLANTUML_JAR="/usr/share/java/plantuml/plantuml.jar"
-    alias llatex="PLANTUML_JAR=${PLANTUML_JAR} lualatex -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error"
+	[ -f "/usr/share/plantuml/plantuml.jar"] PLANTUML_JAR="/usr/share/plantuml/plantuml.jar"
+	[ -f "/usr/share/java/plantuml/plantuml.jar" ] && PLANTUML_JAR="/usr/share/java/plantuml/plantuml.jar"
+	if [ -n "${PLANTUML_JAR}" ]; then
+		alias llatex="lualatex -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error"
+	else
+		alias llatex="PLANTUML_JAR=${PLANTUML_JAR} lualatex -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error"
+	fi
 fi
-
