a3f9d6e52c55b2e01a8eb20ac1d3ceb54e710468
groveer <groveer@outlook.com>
2023-09-07 17:42:00 +0800

    fix(shell): error env for latex

--- shell_env/50-latex.sh
+++ shell_env/50-latex.sh
@@ -3,16 +3,16 @@
 [ -f "/usr/share/java/plantuml/plantuml.jar" ] && PLANTUML_JAR="/usr/share/java/plantuml/plantuml.jar"
 if command -v lualatex >/dev/null 2>&1; then
 	if [ -n "${PLANTUML_JAR}" ]; then
-		alias llatex="lualatex -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error"
-	else
 		alias llatex="PLANTUML_JAR=${PLANTUML_JAR} lualatex -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error"
+	else
+		alias llatex="lualatex -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error"
 	fi
 fi
 
 if command -v xelatex >/dev/null 2>&1; then
 	if [ -n "${PLANTUML_JAR}" ]; then
-		alias xlatex="xelatex -shell-escape"
-	else
 		alias xlatex="PLANTUML_JAR=${PLANTUML_JAR} xelatex -shell-escape"
+	else
+		alias xlatex="xelatex -shell-escape"
 	fi
 fi
