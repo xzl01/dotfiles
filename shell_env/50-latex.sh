7e83b07a4da280908b93aedd5bf7f4e2fa2206b1
groveer <groveer@qq.com>
2023-09-17 15:46:42 +0800

    update latex env

--- shell_env/50-latex.sh
+++ shell_env/50-latex.sh
@@ -1,4 +1,10 @@
 # need texlive-core package
+if [ -d "/usr/local/texlive/" ]; then
+	export MANPATH=$MANPATH:/usr/local/texlive/2023/texmf-dist/doc/man
+	export INFOPATH=$INFOPATH:/usr/local/texlive/2023/texmf-dist/doc/info
+	export PATH=$PATH:/usr/local/texlive/2023/bin/x86_64-linux
+fi
+
 [ -f "/usr/share/plantuml/plantuml.jar" ] && PLANTUML_JAR="/usr/share/plantuml/plantuml.jar"
 [ -f "/usr/share/java/plantuml/plantuml.jar" ] && PLANTUML_JAR="/usr/share/java/plantuml/plantuml.jar"
 if command -v lualatex >/dev/null 2>&1; then
