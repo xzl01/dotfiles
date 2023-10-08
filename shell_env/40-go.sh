43c89011b2c4c236b37f0f0266888c835ca1e968
groveer <groveer@qq.com>
2023-10-08 16:07:54 +0800

    chore: update shell env

--- shell_env/40-go.sh
+++ shell_env/40-go.sh
@@ -1,9 +1,9 @@
 if command -v go >/dev/null 2>&1; then
-    export GOROOT=/usr/lib/go
-    export GOPATH=$HOME/go
-    [ -d "/usr/share/gocode" ] && export GOPATH="$GOPATH:/usr/share/gocode"
-    export GOBIN=$HOME/go/bin
-    export PATH=$PATH:$GOROOT/bin:$GOBIN
-    export GO111MODULE=auto
+	export GOROOT=/usr/lib/go
+	export GOPATH=$HOME/go
+	[ -d "/usr/share/gocode" ] && export GOPATH="$GOPATH:/usr/share/gocode"
+	export GOBIN=$HOME/go/bin
+	export PATH=$PATH:$GOROOT/bin:$GOBIN
+	export GO111MODULE=auto
+	export GOPROXY=https://goproxy.cn,direct
 fi
-
